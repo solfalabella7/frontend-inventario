a07933ce854a94f59bc52ceb75f5316ff3aa5c9f
solfalabella7 <solfalabella7@gmail.com>
2025-05-26 17:37:26 -0300

    Update ListaArticulos.jsx

--- src/components/Articulos/ListaArticulos.jsx
+++ src/components/Articulos/ListaArticulos.jsx
@@ -16,7 +16,13 @@ const ListaArticulos = () => {
   const cargarArticulos = async () => {
     try {
       const respuesta = await axios.get('/articulos'); // Solo agregás la parte del endpoint
-      setArticulos(respuesta.data);
+      //Ordear los articulos: activos primero, luego los dados de baja
+      const articulosOrdenados = respuesta.data.sort((a,b) => {
+        if (a.fechaHoraBajaArticulo && !b.fechaHoraBajaArticulo) return 1;
+        if (!a.fechaHoraBajaArticulo && b.fechaHoraBajaArticulo) return -1;
+        return 0;
+      });
+      setArticulos(articulosOrdenados);
     } catch (err) {
       console.error('Error al obtener los artículos:', err);
       setError('No se pudieron cargar los artículos.');
@@ -40,23 +46,33 @@ const ListaArticulos = () => {
               <th>Descripción</th>
               <th>Stock Actual</th>
               <th>Stock de Seguridad</th>
+              <th>Fecha de Baja</th>
               <th style={{textAlign: 'center'}}>Modificar/Eliminar</th>
             </tr>
           </thead>
           <tbody>
             {articulos.map((articulo) => (
-              <tr key={articulo.codigoArticulo}>
+              <tr key={articulo.codigoArticulo} style={{
+                backgroundColor: articulo.fechaHoraBajaArticulo ? '#FFE0B2' : 'transparent'
+              }}>
                 <td>{articulo.codigoArticulo}</td>
                 <td>{articulo.nombreArticulo}</td>
                 <td>{articulo.descripcion}</td>
                 <td style={{textAlign: 'center'}}>{articulo.stockActualArticulo}</td>
                 <td style={{textAlign: 'center'}}>{articulo.stockSeguridadArticulo}</td>
-                <td style={{display: 'flex', justifyContent:'space-evenly'}}> {/*<i style={{cursor: 'pointer'}} class="bi bi-trash3-fill"></i><i  style={{cursor: 'pointer'}} class="bi bi-pencil-square"></i>*/}
+                <td style={{textAlign: 'center'}}>
+                  {articulo.fechaHoraBajaArticulo 
+                  ? new Date(articulo.fechaHoraBajaArticulo).toLocaleString()
+                  : 'Activo'}
+                </td>
+                <td style={{display: 'flex', justifyContent:'space-evenly',}}> {/*<i style={{cursor: 'pointer'}} class="bi bi-trash3-fill"></i><i  style={{cursor: 'pointer'}} class="bi bi-pencil-square"></i>*/}
                 <button onClick={() => handleEditar(articulo)}>✏️</button>
+                {!articulo.fechaHoraBajaArticulo && (
                   <EliminarArticulo
                     codigoArticulo={articulo.codigoArticulo}
                     onDeleteSuccess={cargarArticulos}
                   />
+                )}
                 </td>
               </tr>
             ))}
